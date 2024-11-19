820996bedab3f13e3fbbc6e2dc884394c95b6747
Norbi0000000 <99403466+Norbi0000000@users.noreply.github.com>
2024-11-19 08:44:11 +0100

    Add files via upload

---
+++ dolgozat.js
@@ -0,0 +1,27 @@
+function createArrayOfTiers (num) {
+    const numStr = '' + num;
+
+        const result = [];
+
+        for (let i = 1; i <= numStr.length; i++) {
+            result.push(numStr.slice(0,i));
+        }
+    return result.sort();
+    return(num);
+}
+console.log(createArrayOfTiers(420));
+console.log(createArrayOfTiers(2017));
+
+function factorial (n) {
+    if (n === 0 || n === 1)
+        return 1;
+
+    return n * factorial(n-1);
+}
+
+function disemvowel(str){
+    return str.replace(/[aeiouAEIOU]/g, '');
+    return str;
+  
+}
+console.log(disemvowel("This website is for losers LOL!"));
